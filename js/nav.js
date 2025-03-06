6c403b4da54d5d9212665a4e5c069ac7bf3fec88
NikolozMokheve <158738357+NikolozGeladze@users.noreply.github.com>
2025-03-06 18:49:43 +0400

    Fixed Navbar on Mobile and Improved Responsiveness for Contact Us Page

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,7 @@
 var menu = document.querySelector('.menu');
 var navBars = document.querySelector('.navigation-bars')
 
+navBars.style.top = document.querySelector('nav').getBoundingClientRect().height - 3 + 'px';
 menu.addEventListener('click', function () {
     navBars.classList.toggle('active');
-    if (navBars.classList.contains('active')) {
-        navBars.style.top = document.querySelector('nav').getBoundingClientRect().height - 3 + 'px';
-    }
 });
